02efdd2a3828e517ad36cdba05359e419e2810ec
Matthew Hooker <mwhooker@gmail.com>
2015-08-24 20:16:48 -0700

    page counter

--- app.py
+++ app.py
@@ -1,16 +1,39 @@
 import requests
 import os
+import json
 
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 
 
 app = Flask(__name__)
 app.debug = True
 
+def request_wants_json():
+    best = request.accept_mimetypes \
+            .best_match(['application/json', 'text/html'])
+    return best == 'application/json' and \
+            request.accept_mimetypes[best] > \
+            request.accept_mimetypes['text/html']
+
+def counter_tpl(counter):
+    alot = ''
+    if counter > 10 ** 9:
+        alot = '<img src="http://cdn.thewritepractice.com/wp-content/uploads/2012/05/Alot-vs-a-lot1-600x450.png" />'
+
+    return """<html>
+<h1>page views</h1>
+%s
+<h2>%s</h2>
+</html>""" % (alot, counter)
+
 @app.route("/")
 def index():
-    msg = requests.get('http://mattapi:5002/').text
-    return jsonify(msg=msg)
+    count_resp = requests.get('http://mattapi:5002/').text
+    counter = int(json.loads(count_resp).get("counter", -1))
+    # counter *= 5 * 10 ** 8
+    if request_wants_json():
+        return jsonify(counter=counter)
+    return counter_tpl(counter)
 
 
 if __name__ == "__main__":
